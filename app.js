f17a23b5e0f843d4928e42024c80597f08cb6acb
RAJSHEKHAR DAS <rajshekhardas85@gmail.com>
2025-02-14 02:37:49 +0530

    socket setup

--- app.js
+++ app.js
@@ -9,10 +9,17 @@ const server= http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine","ejs");
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));  //instructor said app.set
 
 io.on("connection",function(socket){
-    console.log("connected");
+    socket.on("send-location",function(data){
+        io.emit("receive-location",{id:socket.id, ...data});
+    });
+    
+    socket.on("disconnect",function(){
+
+        io.emit("user-disconnected",socket.id);
+    })
 });
 
 app.get("/",function(req,res){
@@ -21,4 +28,5 @@ app.get("/",function(req,res){
 });
 
 server.listen(3000);
+console.log("server running on port 3000");
 
--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ const socket = io();
 
 if(navigator.geolocation){   /*checking if browser supports geolocation, navigator is pre built in the browser*/
         navigator.geolocation.watchPosition((position)=>{
-        const {latitiute, longitude} =position.coords;
+        const {latitude, longitude} =position.coords;
         socket.emit("send-location", {latitude,longitude});
         },
 
@@ -11,12 +11,37 @@ if(navigator.geolocation){   /*checking if browser supports geolocation, navigat
         },
         {
           enableHighAccuracy:true,
-          timeout:5000
-          maximumAge:0,
+          timeout:5000,
+          maximumAge:0
             
         }
     );
 }    
 
-L.map("map").setView([0,0],10);
+const map = L.map("map").setView([0,0],16);
 
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+
+    attribution:"OpenStreetMap"
+}).addTo(map)
+
+const markers={};
+
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+
+    // If marker does not exist, create one
+    if (!markers[id]) {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    } else {
+        // Move existing marker
+        markers[id].setLatLng([latitude, longitude]);
+    }
+
+    // Ensure the map centers on YOUR location
+    if (id === socket.id) {
+        map.setView([latitude, longitude], 16);
+    }
+});
+
+socket.on("disconnect",(id))
